c210101c801985f350f9b0965cb05f9a8b1d074e
Rob Johansen <rob.johansen@gmail.com>
2022-09-29 11:30:41 -0600

    Add a DELETE endpoint and map API keys to tokens

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express, { Request } from 'express'
 
-import { getPrivateKey, getSecrets, insertSecret } from 'src/store'
+import { deleteSecret, getPrivateKey, getSecrets, getTokenSecretMap, insertSecret } from 'src/store'
 import type CreateTokenResponse from 'src/types/CreateTokenResponse' // These should all be renamed to ...Body
 import type GetTokensRequest from 'src/types/GetTokensRequest'
 import type GetTokensResponse from 'src/types/GetTokensResponse'
@@ -28,6 +28,12 @@ doppler.get('/tokens', async (req: Request<{}, GetTokensResponse | string, {}, G
     }
   }
 
+  const tokenSecretMap: Map<string, string> | undefined = await getTokenSecretMap('DEV-API-KEY')
+  if (!tokenSecretMap) {
+    res.status(400).send('Bad Request')
+    return
+  }
+
   const privateKey: string | undefined = await getPrivateKey('DEV-API-KEY')
   if (!privateKey) {
     res.status(400).send('Bad Request')
@@ -35,7 +41,7 @@ doppler.get('/tokens', async (req: Request<{}, GetTokensResponse | string, {}, G
   }
 
   try {
-    const secrets: string[] = await getSecrets(tokens, privateKey)
+    const secrets: string[] = await getSecrets(tokenSecretMap, tokens, privateKey)
 
     if (secrets.length !== tokens.length) {
       res.status(400).send('Bad Request')
@@ -58,6 +64,12 @@ doppler.post('/tokens', async (req: Request<{}, CreateTokenResponse | string, Cr
     return
   }
 
+  const tokenSecretMap: Map<string, string> | undefined = await getTokenSecretMap('DEV-API-KEY')
+  if (!tokenSecretMap) {
+    res.status(400).send('Bad Request')
+    return
+  }
+
   const privateKey: string | undefined = await getPrivateKey('DEV-API-KEY')
   if (!privateKey) {
     res.status(400).send('Bad Request')
@@ -65,7 +77,7 @@ doppler.post('/tokens', async (req: Request<{}, CreateTokenResponse | string, Cr
   }
 
   try {
-    const token = await insertSecret(req.body.secret, privateKey)
+    const token = await insertSecret(tokenSecretMap, req.body.secret, privateKey)
     res.status(201).send({ token })
   } catch (err) {
     res.status(500).send('Internal Server Error')
@@ -74,12 +86,13 @@ doppler.post('/tokens', async (req: Request<{}, CreateTokenResponse | string, Cr
 
 doppler.put('/tokens/:token', async (req: Request<CreateTokenResponse, CreateTokenResponse | string, CreateTokenRequest, {}>, res): Promise<void> => {
   // TODO .... Come back to this when you get a response from Matte.
-  if (!req.params.token) {
+  if (!req.body.secret) {
     res.status(400).send('Bad Request')
     return
   }
 
-  if (!req.body.secret) {
+  const tokenSecretMap: Map<string, string> | undefined = await getTokenSecretMap('DEV-API-KEY')
+  if (!tokenSecretMap) {
     res.status(400).send('Bad Request')
     return
   }
@@ -91,18 +104,31 @@ doppler.put('/tokens/:token', async (req: Request<CreateTokenResponse, CreateTok
   }
 
   try {
-    const [secret] = await getSecrets([req.params.token], privateKey)
+    const [secret] = await getSecrets(tokenSecretMap, [req.params.token], privateKey)
     if (!secret) {
       res.status(400).send('Bad Request')
       return
     }
-  } catch (err) {
 
+    // TODO ....
+  } catch (err) {
+    // TODO ....
   }
 })
 
-doppler.delete('/tokens/:token', (req, res) => {
-  // TODO ....
+doppler.delete('/tokens/:token', async (req: Request<CreateTokenResponse>, res) => {
+  const tokenSecretMap: Map<string, string> | undefined = await getTokenSecretMap('DEV-API-KEY')
+  if (!tokenSecretMap) {
+    res.status(400).send('Bad Request')
+    return
+  }
+
+  try {
+    await deleteSecret(tokenSecretMap, req.params.token)
+    res.status(204).end()
+  } catch (err) {
+    res.status(500).send('Internal Server Error')
+  }
 })
 
 doppler.listen(port, () => {
--- src/store.ts
+++ src/store.ts
@@ -1,21 +1,29 @@
 import { createPrivateKey, decrypt, encrypt } from 'src/crypto'
 import { v4 as uuidv4 } from 'uuid'
 
-// Simulated storage of API keys mapped to private keys
-const API_KEY_MAP: Map<string, string> = new Map([['DEV-API-KEY', createPrivateKey()]])
+// Simulated API key
+const API_KEY = 'DEV-API-KEY'
 
-// Simulated storage of tokens mapped to encrypted secrets
-const TOKEN_SECRET_MAP: Map<string, string> = new Map()
+// Simulated storage of API keys mapped to private keys
+const API_KEY_MAP: Map<string, string> = new Map([[API_KEY, createPrivateKey()]])
+
+// Simulated storage of API keys mapped to tokens and encrypted secrets
+const API_TOKEN_MAP: Map<string, Map<string, string>> = new Map()
+API_TOKEN_MAP.set(API_KEY, new Map())
 
 export const getPrivateKey = (apiKey: string): Promise<string | undefined> => {
   return Promise.resolve(API_KEY_MAP.get(apiKey))
 }
 
-export const getSecrets = (requestedTokens: string[], privateKey: string): Promise<string[]> => {
+export const getTokenSecretMap = (apiKey: string): Promise<Map<string, string> | undefined> => {
+  return Promise.resolve(API_TOKEN_MAP.get(apiKey))
+}
+
+export const getSecrets = (tokenSecretMap: Map<string, string>, requestedTokens: string[], privateKey: string): Promise<string[]> => {
   const secrets: string[] = []
 
   for (const requestedToken of requestedTokens) {
-    const secret = TOKEN_SECRET_MAP.get(requestedToken)
+    const secret = tokenSecretMap.get(requestedToken)
 
     if (secret) {
       secrets.push(decrypt(secret, privateKey))
@@ -25,8 +33,13 @@ export const getSecrets = (requestedTokens: string[], privateKey: string): Promi
   return Promise.resolve(secrets)
 }
 
-export const insertSecret = (secret: string, privateKey: string): Promise<string> => {
+export const insertSecret = (tokenSecretMap: Map<string, string>, secret: string, privateKey: string): Promise<string> => {
   const token = uuidv4().replace(/-/g, '')
-  TOKEN_SECRET_MAP.set(token, encrypt(secret, privateKey))
+  tokenSecretMap.set(token, encrypt(secret, privateKey))
   return Promise.resolve(token)
+}
+
+export const deleteSecret = (tokenSecretMap: Map<string, string>, token: string): Promise<void> => {
+  tokenSecretMap.delete(token)
+  return Promise.resolve()
 }
